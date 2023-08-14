9477fb6114b8aad24aff22a9f8b28fcc9423d32c
dteran24 <dteran21@yahoo.com>
2023-08-14 11:14:27 -0500

    add ionic/react and remove uneeded files

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import { IonHeader, IonPage } from "@ionic/react"
+
+const Home = () => {
+    return (
+        <IonPage>
+            <IonHeader>Home</IonHeader>
+        </IonPage>
+    )
+}
+export default Home;
