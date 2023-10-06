fecd7961a7bacf6850257bb1efb0dd80bd91d4c7
dteran24 <dteran21@yahoo.com>
2023-10-06 12:50:06 -0500

    added android favicion and update url

--- src/services/AxiosInstance.tsx
+++ src/services/AxiosInstance.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosProject = axios.create({
-    baseURL: "http://localhost:8080/workouts"
+    baseURL: "https://liftnotes-service.onrender.com/workouts"
 });
